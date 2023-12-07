40d19dcc799ba4098074af91b4e6d03854b49c07
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2023-12-07 17:06:20 +0300

    first

---
+++ App.js
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+
+    <NavigationContainer>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <View style={{ alignItems: "center", justifyContent: "center", flex: 0.4, marginBottom: 80 }}>
+        <MaterialCommunityIcons name="bus-marker" size={100} color="orange" />
+        <Text style={{
+          color: "#00ADEE",
+          fontWeight: 'bold',
+          fontSize: 50,
+          marginTop: 0,
+        }}>movita</Text>
+        <View style={{ marginTop: 20 }}>
+          <View style={{flexDirection: "row"}}>
+            <Button color="#00ADEE" title='Giriş Yap' onPress={() => alert("butona tıklandı")} />
+            <View style={{ margin: 5 }}></View>
+            <Button color="#00ADEE" title='Kayıt Ol' onPress={() => alert("butona tıklandı")} />
+          </View>
+        </View>
+      </View>
+    </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
