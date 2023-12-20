6c6c28a250201572f5abb65d1e9edc3be08145d6
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2023-12-20 11:34:52 +0300

    new

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './Navigator';
+import 'react-native-gesture-handler'; 
 
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <Navigator/>
+      <Navigator/> 
     </NavigationContainer>
 
   );
--- screens/User.js
+++ screens/User.js
@@ -6,10 +6,11 @@ export const User = () => {
         width: "90%", 
         height: 80, 
         padding: 5, 
-        flexDirection: "row",
+        margin: 3,
+        flexDirection: "row", 
          }}>
-            <View style={{flex: 1 }}></View>
-            <View style={{flex: 1 }}></View>
+            <View style={{flex: 2 }}></View>
+            <View style={{flex: 5 }}></View>
             <View style={{flex: 1 }}></View>
         </View>
     )
