d81c0ee9565907a653434a0a089d7b717c9c6272
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2023-12-20 12:21:42 +0300

    new

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugings: ["react-native-eanimated/plugin"]
+    plugins: ["react-native-eanimated/plugin"],
   };
 };
