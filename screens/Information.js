b92d006094dcb12a156e9c2aa61638b958bb022c
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2023-12-27 17:05:02 +0300

    react-native

--- screens/Information.js
+++ screens/Information.js
@@ -4,10 +4,11 @@ import { View, Text } from "react-native";
 export const Information = () => {
 
     return (
-        <View style={{alignItems: "center", justifycontent: "center"}}>
-            <Text style={{fontSize: 20, fontWeight: "bold"}}>mesai saati</Text>
-            <Text style={{fontSize: 20, fontWeight: "bold"}}>başlangıç</Text>
-            <Text style={{fontSize: 20, fontWeight: "bold"}}>bitiş</Text>
+        <View style={{left: 30, top: 10}}>
+            <Text style={{fontSize: 25, fontWeight: "bold"}}>Mesai Saati</Text>
+            <Text style={{fontSize: 20, fontWeight: "bold"}}>Başlangıç :</Text>
+            <Text style={{fontSize: 20, fontWeight: "bold"}}>Bitiş :</Text>
+            <Text></Text>
         </View>
     );
 };
