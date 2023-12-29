2cae4d8ae612297bdb3c5b9b566c8512d59ed59b
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2023-12-29 11:39:26 +0300

    new

--- screens/Information.js
+++ screens/Information.js
@@ -1,14 +1,51 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Feather } from '@expo/vector-icons';
 
 export const Information = () => {
+    const [showDetails, setShowDetails] = useState(false);
+
+    const toggleDetails = () => {
+        setShowDetails(!showDetails);
+    };
 
     return (
-        <View style={{left: 30, top: 10}}>
-            <Text style={{fontSize: 25, fontWeight: "bold"}}>Mesai Saati</Text>
-            <Text style={{fontSize: 20, fontWeight: "bold"}}>Başlangıç :</Text>
-            <Text style={{fontSize: 20, fontWeight: "bold"}}>Bitiş :</Text>
-            <Text></Text>
+        <View style={{ left: 30, top: 10 }}>
+            <View style={styles.buttonContainer}>
+                <Text style={{ fontSize: 25 }}>Sefer Saati</Text>
+                <TouchableOpacity style={styles.button} onPress={toggleDetails}>
+                    <Feather name={showDetails ? "chevron-down" : "chevron-up"} size={20} color="black" />
+                </TouchableOpacity>
+            </View>
+
+            {showDetails && (
+                <View>
+                    <Text style={{ fontSize: 20 }}>Başlangıç :</Text>
+                    <Text style={{ fontSize: 20 }}>Bitiş :</Text>
+                </View>
+            )}
+
+            <View style={{ margin: 15 }}></View>
+
+            <Text style={{ fontSize: 20 }}>Aracın Gelmesine Kalan Dakika :</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    button: {
+        backgroundColor: "#edebeb",
+        padding: 10,
+        marginLeft: 10,
+        borderRadius: 5,
+        height: 40
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 10,
+    },
+});
