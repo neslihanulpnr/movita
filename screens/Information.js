3b4270255e593ae0693c044771efbd08c61e2ad8
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2024-01-05 13:28:11 +0300

    ınformation

--- screens/Information.js
+++ screens/Information.js
@@ -2,56 +2,51 @@ import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 
 export const Information = ({data}) => {
+  const [userData, setUserData] = useState();
   useEffect(() => {
     console.log("kullanıcı :", data);
 
-    fetch('http://www.movita.com.tr:8019/guzergah_personel_listele', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'sample_token1234'
-      },
-      body: JSON.stringify({user_id:data.ret.user_id})
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('API:', data);
-        // Yeni gelen veriyi state'e kaydedin
-        setUserData(data);
-      })
-      .catch(error => console.error('Hata:', error));
+    const fetchData = async () => {
+      const response = await fetch('http://www.movita.com.tr:8019/guzergah_personel_listele', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'sample_token1234'
+        },
+        body: JSON.stringify({user_id:data.ret.user_id})
+      });
+      const result = await response.json();
+      console.log("RESULT: ",result)
+      setUserData(result.ret);
+    };
+    fetchData();
+      
   }, []); // Boş bağımlılık dizisi
 
-  const [userData, setUserData] = useState();
+  return(
 
-  // userdata tanımlı değilse
-  if (!userData) {
-    return (
-      <View>
-        <Text></Text>
-      </View>
-    );
-  }
-
-  // ret özelliği tanımlı değilse
-  if (userData.ret.length==0) {
-    return (
-      <View>
-        <Text></Text>
-      </View>
-    );
-  }
-
-  return (
     <View>
-      
-      <Text style={{fontSize: 20, margin:15}}>
-        Arac Plakası: {userData.ret[0].arac_plaka} {"\n"}
-        Filo ID: {userData.ret[0].filo_id} {"\n"}
-        Gün: {userData.ret[0].gun} {"\n"}
-        Seans: {userData.ret[0].seans} {"\n"}
-        id: {userData.ret[0].id} {"\n"}
-      </Text>
+      <View style={{margin: 8}} >
+        {userData == undefined ? (
+          <Text>Veri yükleniyor...</Text>
+        ) : (
+          <View>
+            {userData.map(item => (
+                <View >
+                  <Text> PLAKA : {item.arac_plaka} </Text>
+                  <Text> FİLO ID: {item.filo_id} </Text>
+                  <Text> GÜN {item.gun} </Text>
+                  <Text> İSTASYON ID: {item.istasyon_id} </Text>
+                  <Text> SEANS: {item.seans} </Text>
+                  <Text> SMS : {item.sms_atilsin} </Text>
+              </View>
+             
+            ))}
+          </View>
+        )}
+
+      </View>
+    
     </View>
   );
 }
