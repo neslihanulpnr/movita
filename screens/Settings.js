ccaa1c825337dae047b8c6ce33a5726f66bcf6cd
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2024-01-10 12:34:02 +0300

    settings

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,9 +1,23 @@
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
+import { AntDesign } from '@expo/vector-icons';
 
 export const Settings = () => {
-    return(
-        <View style={{justifyContent: "center", alignItems: "center"}}>
-            <Text style={{fontSize: 20, fontWeight: "bold"}}> Ayarlar </Text>
+    const handleForgotPassword = () => {}
+
+
+    return (
+        <View>
+            <TouchableOpacity onPress={handleForgotPassword} 
+            style={{justifyContent: "flex-start", alignItems: "flex-start", margin: 10, flexDirection: "row"}}>
+                <Text style={{ color: "black", fontSize: 25 }}>Şifreyi değiştir</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={handleForgotPassword} 
+            style={{justifyContent: "flex-start", alignItems: "flex-start", margin: 10, flexDirection: "row"}}>
+                <Text style={{ color: "black", fontSize: 25 }}>Şifremi unuttum </Text>
+            </TouchableOpacity>
+
+
         </View>
     )
 }
