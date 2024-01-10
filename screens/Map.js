e43388cf608fe045e4aa57900638a580fd0b0867
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2024-01-10 17:10:21 +0300

    map

--- screens/Map.js
+++ screens/Map.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, image } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 export const Map = ({ data }) => {
   const [userData, setUserData] = useState([]);
-
+  
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchUserData = async () => {
       try {
         const response = await fetch('http://www.movita.com.tr:8019/personel_guzergah_listeleme', {
           method: 'POST',
@@ -17,40 +17,46 @@ export const Map = ({ data }) => {
           body: JSON.stringify({ user_id: data && data.ret && data.ret.user_id })
         });
         const result = await response.json();
-        console.log('API Response:', result.ret);
+        console.log('bilgi API:', result.ret);
         setUserData(result.ret);
+
+        // Gün ve saat karşılaştırması
+        const apiDay = result.ret && result.ret.gun; // API'den gelen gün bilgisi
+        const apiHour = result.ret && result.ret.saat; // API'den gelen saat bilgisi
+        const apiMinutes = result.ret && result.ret.dakika; // API'den gelen dakika bilgisi
+
+        const currentDayNumber = new Date().getDay();
+        const currentHour = new Date().getHours();
+        const currentMinutes = new Date().getMinutes();
+
+        // Gün ve saat karşılaştırması
+        const isDayMatch = (typeof apiDay === 'number' && currentDayNumber === apiDay) ||
+          (typeof apiDay === 'string' && apiDay.toLowerCase() === currentDay.toLowerCase());
+
+        const isTimeMatch = currentHour === apiHour && currentMinutes === apiMinutes;
+
+        // Gün ve saat eşleşiyorsa userData state'ini güncelle
+        if (isDayMatch && isTimeMatch) {
+          console.log('Günler ve saatler eşleşiyor');
+          setUserData(result.ret);
+        } else {
+          console.log('Günler veya saatler eşleşmiyor');
+        }
+
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error bilgi:', error);
       }
     };
-    fetchData();
+
+    fetchUserData();
   }, [data]);
 
-  const turkeyRegion = {
-    latitude: 39.9334,
-    longitude: 32.8597,
-    latitudeDelta: 2,
-    longitudeDelta: 2
-  };
 
-  const markers = [
-    { coordinates: { latitude: 39.9334, longitude: 32.8629 } },
-    { coordinates: { latitude: 39.9434, longitude: 32.8267 } },
-    { coordinates: { latitude: 39.9097, longitude: 32.8401 } },
-    { coordinates: { latitude: 39.8972, longitude: 32.8024 } },
-  ];
 
   return (
     <View>
       <View>
-        <MapView style={styles.map} region={turkeyRegion}>
-          {markers.map((marker, index) => (
-            <Marker
-              key={index}
-              coordinate={marker.coordinates}/>
-              
-          ))}
-        </MapView>
+        <MapView style={styles.map}/>
       </View>
     </View>
   );
