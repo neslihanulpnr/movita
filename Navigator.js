5f5d6b5723ac0e98c147456a161fa51e5a23e4fe
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2024-02-02 11:37:02 +0300

    navigator-settings

--- Navigator.js
+++ Navigator.js
@@ -4,7 +4,8 @@ import { Menu } from "./screens/Menu";
 import { Homepage } from "./screens/Home";
 import { Settings } from "./screens/Settings";
 import { Seferler } from "./screens/Seferler";
-import { Map } from "./screens/Map"
+import { Map } from "./screens/Map";
+
 
 const Stack = createStackNavigator();
 
@@ -12,56 +13,58 @@ export const Navigator = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen
-            name="homepage"
-            component={Homepage}
-            options={{
-            title: "",
-            headerStyle: {
-            height: 1
-            }
-            }}/>
+                name="homepage"
+                component={Homepage}
+                options={{
+                    title: "",
+                    headerStyle: {
+                        height: 1
+                    }
+                }} />
 
             <Stack.Screen name="login"
-            component={Login}/>
+                component={Login} />
 
             <Stack.Screen name="menu"
-            component={Menu}
-            options={{
-            title: "",
-            headerStyle: {
-                height: 1,
-                backgroundColor: "orange"
-                }
-            }}/>
+                component={Menu}
+                options={{
+                    title: "",
+                    headerStyle: {
+                        height: 1,
+                        backgroundColor: "orange"
+                    }
+                }} />
 
             <Stack.Screen name="Settings"
-            component={Settings}
-            options={{
-            title: "",
-            headerStyle: {
-                height: 1,
-                backgroundColor: "orange"
-                }
-            }}/>
+                component={Settings}
+                options={{
+                    title: "",
+                    headerStyle: {
+                        height: 1,
+                        backgroundColor: "orange"
+                    }
+                }} />
 
             <Stack.Screen name="Map"
-            component={Map}
-            options={{
-            title: "",
-            headerShown: false,
-            headerStyle: {
-                backgroundColor: "#edebeb"
-                }
-            }}/>
+                component={Map}
+                options={{
+                    title: "",
+                    headerShown: false,
+                    headerStyle: {
+                        backgroundColor: "#edebeb"
+                    }
+                }} />
 
             <Stack.Screen name="Seferler"
-            component={Seferler}
-            options={{
-            title: "",
-            headerStyle: {
-                backgroundColor: "#edebeb"
-                }
-            }}/>
+                component={Seferler}
+                options={{
+                    title: "",
+                    headerStyle: {
+                        backgroundColor: "#edebeb"
+                    }
+                }} />
+
+
 
         </Stack.Navigator>
     )
--- screens/Settings.js
+++ screens/Settings.js
@@ -1,54 +1,132 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import MapView, { Marker } from "react-native-maps";
 
-export const Settings = ({ data }) => {
-  const filoData = data?.ret?.filo;
+export const Settings = () => {
+  const [ilValue, setIlValue] = useState("");
+  const [ilceValue, setIlceValue] = useState("");
+  const [mahalleValue, setMahalleValue] = useState("");
+  const [sokakValue, setSokakValue] = useState("");
+  const [showMap, setShowMap] = useState(false);
+
+  const logEnteredInformation = () => {
+    console.log("Girilen Bilgiler:");
+    console.log("İl:", ilValue);
+    console.log("İlçe:", ilceValue);
+    console.log("Mahalle:", mahalleValue);
+    console.log("Sokak:", sokakValue);
+    setShowMap(true);
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Kişi Bilgileri</Text>
+    <View>
+      <View style={styles.inputContainer}>
+        <View style={styles.input}>
+          <TextInput
+            placeholder="İl"
+            placeholderTextColor={"gray"}
+            style={{ flex: 1, marginLeft: 10 }}
+            value={ilValue}
+            onChangeText={(text) => setIlValue(text)}
+          />
+        </View>
+
+        <View style={styles.input}>
+          <TextInput
+            placeholder="İlçe"
+            placeholderTextColor={"gray"}
+            style={{ flex: 1, marginLeft: 10 }}
+            value={ilceValue}
+            onChangeText={(text) => setIlceValue(text)}
+          />
+        </View>
+
+        <View style={styles.input}>
+          <TextInput
+            placeholder="Mahalle"
+            placeholderTextColor={"gray"}
+            style={{ flex: 1, marginLeft: 10 }}
+            value={mahalleValue}
+            onChangeText={(text) => setMahalleValue(text)}
+          />
+        </View>
+
+        <View style={styles.input}>
+          <TextInput
+            placeholder="Sokak"
+            placeholderTextColor={"gray"}
+            style={{ flex: 1, marginLeft: 10 }}
+            value={sokakValue}
+            onChangeText={(text) => setSokakValue(text)}
+          />
+        </View>
+
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#00ADEE",
+            padding: 10,
+            borderRadius: 5,
+            width: 170,
+            height: 40,
+          }}
+          onPress={() => {
+            logEnteredInformation();
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontWeight: "bold",
+            }}
+          >
+            Harita üzerinde göster
+          </Text>
+        </TouchableOpacity>
       </View>
 
-      <View style={styles.content}>
-        {filoData && (
-          <View>
-            <Text style={styles.infoText}>Adres: {filoData.address}</Text>
-            <Text style={styles.infoText}>E-posta: {filoData.eposta}</Text>
-            <Text style={styles.infoText}>Telefon: {filoData.telefon}</Text>
-          </View>
-        )}
-      </View>
+      {showMap && (
+        <View>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: 39.933365,
+              longitude: 32.859741,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            <Marker
+              coordinate={{ latitude: 39.933365, longitude: 32.859741 }}
+              title=""
+            />
+          </MapView>
+        </View>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    margin: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 10,
+  inputContainer: {
+    alignItems: "center",
+    justifyContent: "center",
     padding: 10,
   },
-  header: {
-    justifyContent: "center",
+  input: {
+    flexDirection: "row",
     alignItems: "center",
-    marginBottom: 10,
+    width: 300,
+    height: 55,
+    padding: 15,
+    borderWidth: 0.2,
+    borderRadius: 2,
+    margin: 5,
   },
-  headerText: {
-    fontSize: 30,
-    fontWeight: "bold",
-  },
-  content: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  infoText: {
-    color: "black",
-    fontSize: 23,
-    marginBottom: 5,
+  map: {
+    height: 370,
+    width: 400,
+    position: "absolute",
+    top: 10
   },
 });
-
-export default Settings;
